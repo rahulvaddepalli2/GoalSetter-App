429d23a96e41967e8c84e698f5aa410bb5666f44
Rahul Vaddepalli <rahulvaddepalli2@gmail.com>
2022-02-25 18:39:23 -0800

    REST API for goals

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -10,14 +10,6 @@ const {
 // Specifies routes for when router calls the RESTful functions we created (ie, getGoals, setGoals)
 
 router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').put(updateGoal).delete(deleteGoal)
-
-router.get(getGoals)
-
-router.post(setGoal)
-
-router.put(updateGoal)
-
-router.delete(deleteGoal)
+router.route('/:id').delete(deleteGoal).put(updateGoal)
 
 module.exports = router
